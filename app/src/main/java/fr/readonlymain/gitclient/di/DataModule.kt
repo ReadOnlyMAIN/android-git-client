dcd87a4fef2015ba8a9b924b97532526e2b8a05f
ReadOnlyMAIN <70072185+ReadOnlyMAIN@users.noreply.github.com>
2026-04-27 13:44:09 +0200

    feat: add fetch system

--- app/src/main/java/fr/readonlymain/gitclient/di/DataModule.kt
+++ app/src/main/java/fr/readonlymain/gitclient/di/DataModule.kt
@@ -6,7 +6,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import fr.readonlymain.gitclient.data.preferences.CredentialsPreferences
 import fr.readonlymain.gitclient.data.preferences.RepositoriesPreferences
+import fr.readonlymain.gitclient.data.preferences.ThemePreferences
 import javax.inject.Singleton
 
 @Module
@@ -17,5 +19,17 @@ object DataModule {
     @Singleton
     fun provideRepositoriesPreferences(@ApplicationContext context: Context): RepositoriesPreferences {
         return RepositoriesPreferences(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCredentialsPreferences(@ApplicationContext context: Context): CredentialsPreferences {
+        return CredentialsPreferences(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideThemePreferences(@ApplicationContext context: Context): ThemePreferences {
+        return ThemePreferences(context)
     }
 }
